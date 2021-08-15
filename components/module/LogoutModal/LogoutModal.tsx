bd6e1d3082f559ce2c2b5738215226352502ad47
Martin Pilaszek <martinpilaszek@gmail.com>
2021-08-15 19:05:59 -0300

    terminado el boton de logout de la barra de navegacion

---
+++ components/module/LogoutModal/LogoutModal.tsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react"
+
+import { Modal, View } from "react-native"
+import Toast from "react-native-root-toast"
+import { useHistory } from "react-router-native"
+import { firebaseAuth } from "../../../firebase/firebase"
+import Button from "../../common/Button"
+import StyledText from "../../common/StyledText"
+
+interface Props {
+  visible: boolean
+  setVisible: (value: boolean) => void
+}
+
+const LogoutModal: React.FC<Props> = ({ visible, setVisible }) => {
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false)
+  const history = useHistory()
+  const handleLogOut = async () => {
+    return await firebaseAuth
+      .signOut()
+      .then(() => {
+        history.push("/welcome")
+        setIsSubmitting(false)
+      })
+      .catch((err) => {
+        // eslint-disable-next-line no-unused-vars
+        const toast = Toast.show(err.message, {
+          duration: Toast.durations.LONG,
+          backgroundColor: "red",
+          position: Toast.positions.TOP + 25,
+          shadow: false,
+          opacity: 1,
+        })
+        setIsSubmitting(false)
+      })
+  }
+  return (
+    <Modal transparent={true} animationType="fade" visible={visible}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+        }}
+      >
+        <View
+          style={{
+            padding: 20,
+            borderRadius: 10,
+            backgroundColor: "white",
+            shadowColor: "#000",
+            shadowOffset: { width: 0, height: 25 },
+            shadowOpacity: 0.25,
+            elevation: 15,
+            marginHorizontal: 20,
+          }}
+        >
+          <StyledText center>Are you sure you want to log out?</StyledText>
+          <View
+            style={{
+              flexDirection: "row",
+              marginTop: 15,
+              paddingHorizontal: 20,
+            }}
+          >
+            <Button
+              isLoading={isSubmitting}
+              style={{ flex: 1, marginRight: 5 }}
+              onPress={() => setVisible(false)}
+              ghostPrimary
+            >
+              Cancel
+            </Button>
+            <Button
+              isLoading={isSubmitting}
+              onPress={handleLogOut}
+              style={{ flex: 1 }}
+              primary
+            >
+              Log out
+            </Button>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  )
+}
+
+export default LogoutModal
--- components/module/NavigationBar/NavigationBar.tsx
+++ components/module/NavigationBar/NavigationBar.tsx
@@ -1,14 +1,16 @@
-import React from "react"
+import React, { useState } from "react"
 import { View } from "react-native"
 import { useLocation } from "react-router-native"
 import { MaterialIcons } from "@expo/vector-icons"
 import theme from "../../../theme/theme"
+import LogoutModal from "../LogoutModal/LogoutModal"
 
 interface Props {
   style?: any
 }
 
 const NavigationBar: React.FC<Props> = ({ style }) => {
+  const [modalVisible, setModalVisible] = useState<boolean>(false)
   const { pathname } = useLocation()
   return (
     <View
@@ -23,10 +25,12 @@ const NavigationBar: React.FC<Props> = ({ style }) => {
         style,
       ]}
     >
+      <LogoutModal setVisible={setModalVisible} visible={modalVisible} />
       <MaterialIcons
         name="logout"
         size={30}
         color={theme.colors.primary[500]}
+        onPress={() => setModalVisible(true)}
       />
       <MaterialIcons name="timer" size={30} color={theme.colors.primary[500]} />
       <MaterialIcons
